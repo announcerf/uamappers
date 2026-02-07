e66628087c3810a77584f4fc5a4ee714d6e01134
hiyashinsu <esutarosa.git@gmail.com>
2026-02-08 01:59:01 +0200

    feat(api): update app state

--- apps/api/src/app/state.rs
+++ apps/api/src/app/state.rs
@@ -1,11 +1,15 @@
 use sea_orm::DatabaseConnection;
 
-use crate::features::{ingest::storage::repo::ScanStateRepo, mappers::storage::repo::MapperRepo};
+use crate::features::{
+    ingest::storage::repo::ScanStateRepo,
+    mappers::storage::{beatmapset_repo::BeatmapsetRepo, repo::MapperRepo},
+};
 
 #[derive(Clone)]
 pub struct AppState {
     pub db: DatabaseConnection,
     pub mappers_repo: MapperRepo,
+    pub beatmapsets_repo: BeatmapsetRepo,
     pub scan_state_repo: ScanStateRepo,
 }
 
@@ -13,6 +17,7 @@ impl AppState {
     pub fn new(db: DatabaseConnection) -> Self {
         Self {
             mappers_repo: MapperRepo::new(db.clone()),
+            beatmapsets_repo: BeatmapsetRepo::new(db.clone()),
             scan_state_repo: ScanStateRepo::new(db.clone()),
             db,
         }
