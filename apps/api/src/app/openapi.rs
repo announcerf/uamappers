9aad0c07629ef4e88360496d9dd3a0aa7e069af3
hiyashinsu <esutarosa.git@gmail.com>
2026-02-08 01:59:01 +0200

    feat(openapi): update openapi

--- apps/api/src/app/openapi.rs
+++ apps/api/src/app/openapi.rs
@@ -2,7 +2,10 @@ use utoipa::OpenApi;
 
 use crate::features::{
     ingest::http::dto::{IngestStatusDtoV1, ScanStateDtoV1},
-    mappers::http::dto::{MapperDtoV1, MapperListQuery, MapperListResponseV1, MapperSearchQuery},
+    mappers::http::dto::{
+        MapperBeatmapsetDtoV1, MapperBeatmapsetListQuery, MapperBeatmapsetListResponseV1,
+        MapperDtoV1, MapperListQuery, MapperListResponseV1, MapperSearchQuery,
+    },
     system::http::dto::HealthDtoV1,
 };
 use crate::shared::errors::ErrorResponse;
@@ -10,9 +13,12 @@ use crate::shared::errors::ErrorResponse;
 #[derive(OpenApi)]
 #[openapi(
     paths(
-        crate::features::mappers::http::handlers::list_mappers,
-        crate::features::mappers::http::handlers::get_mapper,
-        crate::features::mappers::http::handlers::search_mappers,
+        crate::features::mappers::http::handlers::list::list_mappers,
+        crate::features::mappers::http::handlers::get::get_mapper,
+        crate::features::mappers::http::handlers::get::get_mapper_by_id,
+        crate::features::mappers::http::handlers::list::search_mappers,
+        crate::features::mappers::http::handlers::beatmapsets::list_mapper_beatmapsets,
+        crate::features::mappers::http::handlers::beatmapsets::list_mapper_beatmapsets_by_id,
         crate::features::ingest::http::handlers::get_status,
         crate::features::system::http::handlers::health
     ),
@@ -21,6 +27,9 @@ use crate::shared::errors::ErrorResponse;
         MapperListResponseV1,
         MapperListQuery,
         MapperSearchQuery,
+        MapperBeatmapsetDtoV1,
+        MapperBeatmapsetListQuery,
+        MapperBeatmapsetListResponseV1,
         IngestStatusDtoV1,
         ScanStateDtoV1,
         HealthDtoV1,
@@ -29,7 +38,10 @@ use crate::shared::errors::ErrorResponse;
     tags(
         (name = "Mappers::List"),
         (name = "Mappers::Get"),
+        (name = "Mappers::GetById"),
         (name = "Mappers::Search"),
+        (name = "Mappers::Beatmapsets"),
+        (name = "Mappers::BeatmapsetsById"),
         (name = "Ingest::Status"),
         (name = "System::Health")
     )
