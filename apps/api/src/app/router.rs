8bd46b35f441a58e17dd68fb7e40e4764af954af
hiyashinsu <esutarosa.git@gmail.com>
2026-02-09 03:24:08 +0200

    fix(api): remove overlapping /mappers route

--- apps/api/src/app/router.rs
+++ apps/api/src/app/router.rs
@@ -10,10 +10,6 @@ use crate::{
 
 pub fn build(state: AppState) -> Router {
     let api_router = Router::new()
-        .route(
-            "/mappers",
-            axum::routing::get(mappers::http::handlers::list_mappers),
-        )
         .nest("/mappers", mappers::routes::router())
         .nest("/ingest", ingest::routes::router())
         .nest("/system", system::routes::router())
