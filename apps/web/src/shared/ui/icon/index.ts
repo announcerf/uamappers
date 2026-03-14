b2f19012db8d7b23829b0edef985eac3b51b9642
hiyashinsu <esutarosa.git@gmail.com>
2026-03-14 04:07:52 +0200

    feat(web): add osu standard icon

--- apps/web/src/shared/ui/icon/index.ts
+++ apps/web/src/shared/ui/icon/index.ts
@@ -1,4 +1,5 @@
 export { default as BaseIcon } from '@/shared/ui/icon/ui/BaseIcon.vue';
+export { default as Icon } from '@/shared/ui/icon/ui/BaseIcon.vue';
 export { iconProps } from '@/shared/ui/icon/model/props';
 export type { BaseIconProps } from '@/shared/ui/icon/model/types';
 export { useIcon } from '@/shared/ui/icon/model/useIcon';
