9edc6f11f7394ec11df31a516413c4d2b519ea3e
announcerf <tasaneono22@gmail.com>
2026-03-14 17:17:17 +0200

    feat(web): add osu gamemodes icons and main page icons

--- apps/web/src/shared/icons/index.ts
+++ apps/web/src/shared/icons/index.ts
@@ -1,1 +1,11 @@
 export * from '@/shared/icons/osu/OsuStandardIcon.vue';
+export * from '@/shared/icons/osu/OsuManiaIcon.vue';
+export * from '@/shared/icons/osu/OsuTaikoIcon.vue';
+export * from '@/shared/icons/osu/OsuCatchIcon.vue';
+
+export * from '@/shared/icons/arrows/CaretDownIcon.vue';
+export * from '@/shared/icons/arrows/ArrowsCounterClockwiseIcon.vue';
+
+export * from '@/shared/icons/other/GithubLogoIcon.vue';
+export * from '@/shared/icons/other/MagnifyingGlassIcon.vue';
+export * from '@/shared/icons/other/TranslateIcon.vue';
